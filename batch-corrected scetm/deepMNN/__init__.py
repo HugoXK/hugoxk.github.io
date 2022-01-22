8a56babacc9c41598d22bca890d5b70b2fa6f032
HugoXK <ke.xu@zju.edu.cn>
2022-01-21 20:09:44 -0600

    move batch-corrected scetm to another repository

--- batch-corrected scetm/deepMNN/__init__.py
+++
@@ -1,3 +0,0 @@
-"""deepMNN - Deep learning based single-cell RNA-seq batch correction using mutual nearest neighbors"""
-
-from .deep_batch_correction import correct_scanpy
--- batch-corrected scetm/src/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .scETM import scETM
-from .scVI import scVI
-from .BaseCellModel import BaseCellModel
-from .model_utils import get_fully_connected_layers, get_kl
